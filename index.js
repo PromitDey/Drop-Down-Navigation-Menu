94f2ac7f5354c5ef16bcb995f81e95a6b855cf2b
Promit Dey <promitdey140@gmail.com>
2022-04-22 21:38:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function features()
+{
+    document.getElementById("items1").classList.toggle("dropdown-content-1");
+}
+
+function company()
+{
+    document.getElementById("items2").classList.toggle("dropdown-content-2");
+}
